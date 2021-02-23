6adc63cf123a358e45ae2ef29f56c8bec773eba1
cHiiisel <chisel_112@outlook.com>
2021-02-24 03:57:21 +0800

    提交了MQTT的beta1.0版本代码；修改了部分esp8266细节；调节了文件夹结构

---
+++ STM32Lib/ESP8266&&MQTT/MQTT/Include/transport.h
@@ -0,0 +1,22 @@
+#ifndef __transport_H
+#define __transport_H
+
+#include "stm32f4xx_hal.h"
+#include "my_uart.h"
+#include "esp8266_at.h"
+#include "IOT_ali.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define UART_RX_BUF_SIZE	1024
+
+/* º¯ÊıÉùÃ÷ */
+int transport_sendPacketBuffer(int sock, unsigned char* buf, int buflen);
+int transport_getdata(unsigned char* buf, int count);
+int transport_getdatanb(void *sck, unsigned char* buf, int count);
+int transport_open(char* addr, char* port);
+int transport_close(int sock);
+
+
+#endif  /* __transport_H */
